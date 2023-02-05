d8078944c6f79bc81de25016bb1ce32764b2cfcb
Dongning Song <mathdsong@gmail.com>
2023-02-05 16:04:13 -0700

    Time: 4940 ms (5.03%), Space: 48.8 MB (15.13%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    var obj = {}, res = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (Object.keys(obj).includes(nums[i].toString())) {
+            obj[nums[i]]++;
+        } else {
+            obj[nums[i]] = 1;
+        }
+    }
+
+    let sorted = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+    for (var i = 0; i < k; i++) {
+        res.push(sorted[i][0]);
+    }
+
+    return res;
+};
