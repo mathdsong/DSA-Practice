3908f890a87b0f8c295831aae6bb6cac26326872
Dongning Song <mathdsong@gmail.com>
2023-02-05 16:57:37 -0700

    Time: 85 ms (67.06%), Space: 44.8 MB (68.09%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -3,16 +3,18 @@
  * @param {number} k
  * @return {number[]}
  */
+
 var topKFrequent = function(nums, k) {
     var obj = {}, res = [];
     for (var i = 0; i < nums.length; i++) {
-        if (Object.keys(obj).includes(nums[i].toString())) {
+        if (obj.hasOwnProperty(nums[i])) {
             obj[nums[i]]++;
         } else {
             obj[nums[i]] = 1;
         }
     }
 
+    // let sorted = Object.keys(obj).map(key => [key, obj[key]]).sort((a, b) => b[1] - a[1]);
     let sorted = Object.entries(obj).sort((a, b) => b[1] - a[1]);
     for (var i = 0; i < k; i++) {
         res.push(sorted[i][0]);
@@ -20,3 +22,23 @@ var topKFrequent = function(nums, k) {
 
     return res;
 };
+
+
+// var topKFrequent = function(nums, k) {
+//     var obj = {}, res = [];
+//     for (var i = 0; i < nums.length; i++) {
+//         if (Object.keys(obj).includes(nums[i].toString())) {
+//             obj[nums[i]]++;
+//         } else {
+//             obj[nums[i]] = 1;
+//         }
+//     }
+
+//     let sorted = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+//     for (var i = 0; i < k; i++) {
+//         res.push(sorted[i][0]);
+//     }
+
+//     return res;
+// };
+
