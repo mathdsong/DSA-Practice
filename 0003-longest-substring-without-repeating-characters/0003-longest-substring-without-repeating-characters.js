12a3f11e68c1825c61f6aaba50a94afcd13a3f8b
Dongning Song <mathdsong@gmail.com>
2023-02-17 08:48:57 -0700

    Time: 132 ms (38.66%), Space: 49.9 MB (14.78%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,22 +2,59 @@
  * @param {string} s
  * @return {number}
  */
+// 2nd solution:
 var lengthOfLongestSubstring = function(s) {
-    let left = 0, currStr = ''; res = 0;
-    for (let right = 0; right < s.length; right++) {
-        if (!currStr.includes(s[right])) {
-            currStr += s[right];
-            res = Math.max(res, currStr.length);
-        } else {
-            res = Math.max(res, currStr.length);
-            while (s[left] !== s[right] && left < right) {
-                left++;
+    let left = 0, max = 0, hashmap = new Map();
+    for (var right = 0; right < s.length; right++) {
+        if (hashmap.has(s[right])) {
+            if (hashmap.get(s[right]) < left) {
+                max = Math.max(max, right - left + 1);
+            } else {
+                max = Math.max(max, right - left);
+                left = hashmap.get(s[right]) + 1;
             }
-            currStr = '';
-            left++;
-            right = left - 1;
+        } else {
+            max = Math.max(max, right - left + 1);
         }
-        
+        hashmap.set(s[right], right);
     }
-    return res;
-};
+    console.log(hashmap)
+    return max;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1st solution:
+// var lengthOfLongestSubstring = function(s) {
+//     let left = 0, currStr = ''; res = 0;
+//     for (let right = 0; right < s.length; right++) {
+//         if (!currStr.includes(s[right])) {
+//             currStr += s[right];
+//             res = Math.max(res, currStr.length);
+//         } else {
+//             res = Math.max(res, currStr.length);
+//             while (s[left] !== s[right] && left < right) {
+//                 left++;
+//             }
+//             currStr = '';
+//             left++;
+//             right = left - 1;
+//         }
+        
+//     }
+//     return res;
+// };
