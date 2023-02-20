c6ec13fa67f73d1c1d1dad0d6b618c3a97a49bb6
Dongning Song <mathdsong@gmail.com>
2023-02-20 08:58:26 -0700

    Time: 75 ms, Memory: 41.7 MB - LeetHub

---
+++ middle-of-the-linked-list/middle-of-the-linked-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// var middleNode = function(head) {
+//     let slow = head;
+//     let fast = head;
+    
+//     while (fast.next) {
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }
+//     return slow;
+// };
+var middleNode = function(head) {
+    slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
