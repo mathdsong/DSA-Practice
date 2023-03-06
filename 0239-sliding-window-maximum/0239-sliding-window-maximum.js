1acf7361c4435d1f504cc59dabda8da901881988
Dongning Song <mathdsong@gmail.com>
2023-03-06 01:00:54 -0700

    Time: 809 ms (26.48%), Space: 76.4 MB (50.50%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let queue = [], result = [];
+    for (var i = 0; i < nums.length; i++) {
+        if (queue.length) {
+            while (i - queue[0] > k - 1) {
+                queue.shift();
+            }
+            while (nums[i] > nums[queue[queue.length - 1]]) {
+                queue.pop();
+            }
+            queue.push(i);
+            if (i >= k - 1) {
+                result.push(nums[queue[0]]);
+            }
+        } else {
+            queue.push(i);
+            if (k === 1) {
+                result.push(nums[queue[0]]);
+                queue.pop();
+            }
+        }
+    }
+    return result;
+};
