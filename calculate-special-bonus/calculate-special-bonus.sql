ba061e0c698acad762765c35123f991350fcb812
Dongning Song <mathdsong@gmail.com>
2023-03-30 14:26:01 -0600

    Time: 1129 ms, Memory: 0B - LeetHub

--- calculate-special-bonus/calculate-special-bonus.sql
+++ calculate-special-bonus/calculate-special-bonus.sql
@@ -6,7 +6,7 @@
 SELECT `employee_id`, 
     CASE
         WHEN `employee_id` % 2 = 1 AND `name` NOT LIKE 'M%' THEN `salary`
-        ELSE `salary` = 0
+        ELSE 0
     END
     AS `bonus`
     FROM Employees
