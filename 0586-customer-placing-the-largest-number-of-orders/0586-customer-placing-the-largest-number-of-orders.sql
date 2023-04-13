3c9a710bb6e14a2ceeba8d958700dbcb48515dd0
Dongning Song <mathdsong@gmail.com>
2023-04-13 11:58:58 -0600

    Time: 974 ms (43.53%), Space: 0B (100.00%) - LeetHub

---
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+# Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+# The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+
+SELECT DISTINCT `customer_number` FROM Orders
+WHERE `customer_number` = (
+    SELECT `customer_number` FROM (
+        SELECT `customer_number`, COUNT(`order_number`) FROM Orders
+        GROUP BY `customer_number` 
+        HAVING COUNT(`order_number`) = (
+                SELECT COUNT(`order_number`) FROM Orders 
+                GROUP BY `customer_number` 
+                ORDER BY COUNT(`order_number`) DESC 
+                LIMIT 1
+        )
+    ) New
+);
+
