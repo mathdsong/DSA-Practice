3f5c05b8991e04c56748e1f74d1796eafdbcf4ce
Dongning Song <mathdsong@gmail.com>
2023-04-13 12:02:46 -0600

    Time: 840 ms (82.91%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -2,24 +2,24 @@
 # Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
 # The test cases are generated so that exactly one customer will have placed more orders than any other customer.
 
-# it works but not brief:
-SELECT DISTINCT `customer_number` FROM Orders
-WHERE `customer_number` = (
-    SELECT `customer_number` FROM (
-        SELECT `customer_number`, COUNT(`order_number`) FROM Orders
-        GROUP BY `customer_number` 
-        HAVING COUNT(`order_number`) = (
-                SELECT COUNT(`order_number`) FROM Orders 
-                GROUP BY `customer_number` 
-                ORDER BY COUNT(`order_number`) DESC 
-                LIMIT 1
-        )
-    ) New
-);
+# # it works but not brief:
+# SELECT DISTINCT `customer_number` FROM Orders
+# WHERE `customer_number` = (
+#     SELECT `customer_number` FROM (
+#         SELECT `customer_number`, COUNT(`order_number`) FROM Orders
+#         GROUP BY `customer_number` 
+#         HAVING COUNT(`order_number`) = (
+#                 SELECT COUNT(`order_number`) FROM Orders 
+#                 GROUP BY `customer_number` 
+#                 ORDER BY COUNT(`order_number`) DESC 
+#                 LIMIT 1
+#         )
+#     ) New
+# );
 
-# SELECT `customer_number`
-# FROM Orders
-# GROUP BY `customer_number`
-# ORDER BY COUNT(`order_number`) DESC
-# LIMIT 1;
+SELECT `customer_number`
+FROM Orders
+GROUP BY `customer_number`
+ORDER BY COUNT(`order_number`) DESC
+LIMIT 1;
 
