e690d8e73e9eff2c9dbbf2e89ca6910a9c6a31e0
Dongning Song <mathdsong@gmail.com>
2023-04-13 13:29:34 -0600

    Time: 1061 ms, Memory: 0B - LeetHub

---
+++ employees-with-missing-information/employees-with-missing-information.sql
@@ -0,0 +1,38 @@
+# Write your MySQL query statement below
+
+# Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+# The employee's name is missing, or
+# The employee's salary is missing.
+# Return the result table ordered by employee_id in ascending order.
+
+# SELECT `employee_id` FROM Employees
+# WHERE `employee_id` IN (
+#     SELECT `employee_id` FROM (
+#         SELECT e.`employee_id`, e.`name`, s.`salary` FROM Employees e
+#         LEFT JOIN Salaries s ON e.`employee_id` = s.`employee_id`
+#         UNION 
+#         SELECT s.`employee_id`, e.`name`, s.`salary` FROM Employees e
+#         RIGHT JOIN Salaries s ON e.`employee_id` = s.`employee_id`  
+#     ) New 
+#     WHERE `name` IS NULL OR `salary` IS NULL
+# ) 
+
+
+# SELECT e.`employee_id`, e.`name`, s.`salary` FROM Employees e
+# LEFT JOIN Salaries s ON e.`employee_id` = s.`employee_id`
+# UNION 
+# SELECT s.`employee_id`, e.`name`, s.`salary` FROM Employees e
+# RIGHT JOIN Salaries s ON e.`employee_id` = s.`employee_id`
+
+SELECT `employee_id` FROM (
+    SELECT e.`employee_id`, e.`name`, s.`salary` FROM Employees e
+    LEFT JOIN Salaries s ON e.`employee_id` = s.`employee_id`
+    UNION 
+    SELECT s.`employee_id`, e.`name`, s.`salary` FROM Employees e
+    RIGHT JOIN Salaries s ON e.`employee_id` = s.`employee_id`  
+) New 
+WHERE `name` IS NULL OR `salary` IS NULL
+ORDER BY `employee_id`;
+
+
