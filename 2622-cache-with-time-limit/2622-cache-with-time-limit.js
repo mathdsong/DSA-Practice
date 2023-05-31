abdf8751f5c99a86ee1af7b08f428f049a0af7bd
Dongning Song <mathdsong@gmail.com>
2023-05-31 14:40:35 -0600

    Time: 52 ms (91.94%), Space: 41.7 MB (74.69%) - LeetHub

---
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.js
@@ -0,0 +1,51 @@
+
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+
+    let createNew = () => {
+        let newTimeout = setTimeout(() => this.cache.delete(key), duration);
+        this.cache.set(key, {val: value, tmt: newTimeout});
+    }
+
+    if (this.cache.has(key)) {
+        clearTimeout(this.cache.get(key).tmt);
+        createNew();
+        return true;
+    }
+
+    createNew();
+    return false;
+
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key) ? this.cache.get(key).val : -1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * Your TimeLimitedCache object will be instantiated and called as such:
+ * var obj = new TimeLimitedCache()
+ * obj.set(1, 42, 1000); // false
+ * obj.get(1) // 42
+ * obj.count() // 1
+ */
