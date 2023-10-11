c16bbd1ec877c28436d040b92371fc04cc874691
Dongning Song <mathdsong@gmail.com>
2023-10-11 13:39:24 -0600

    Time: 51 ms (64.64%), Space: 42 MB (58.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,33 +5,27 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
+
 var merge = function(nums1, m, nums2, n) {
-    // initialize two pointers for two arrays resp
-    let p1 = 0, p2 = 0; 
-    // loop through nums2:
+    let p1 = 0, p2 = 0;
     while (p2 < nums2.length) {
-        // if the current number in nums1 is smaller than the current number in nums2, then there are two cases:
         while (nums1[p1] < nums2[p2]) {
-            // case 1: if hitting the last number of nums1 that should be merged with value 0:
-            if (nums1[p1] === 0 && p1 >= m + p2) {
-                // then we just reassign the rest of 0s left in nums1 by the rest of nums2 resp, afterwards, jump out of the program
-                for (let i = p1; i < nums1.length; i++) {
-                    nums1[p1] = nums2[p2];
+            // if we just hit the 1st 0 that doesn't belong to the "should be merged subset of nums1", then replace all left 0s by the remaining numbers of nums2
+            if (p1 === m + p2) {
+                for (let i = p2; i < nums2.length; i++) {
+                    nums1[p1] = nums2[i];
                     p1++;
-                    p2++;
                 }
                 break;
             }
-            // case 2: 
+            // otherwise, increase p1 by 1
             p1++;
         }
-        // if the current number in nums1 is greater than or equal to the current number in nums2
-        // then insert this current number in nums2 to nums1 at the index of the current number in nums1
+        // once the current number in nums1 >= nums2's, then insert nums2's number into the p1's position
+        // remove the last 0 from nums1 to keep the same length as we just inserted, p1 -> p1 + 1, p2 -> p2 + 1
         nums1.splice(p1, 0, nums2[p2]);
         nums1.pop();
-        p2++;
         p1++;
-
+        p2++;
     }
-
 };
