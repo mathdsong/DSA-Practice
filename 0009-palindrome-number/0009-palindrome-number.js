beca6b1281c95970fbd06652b0d7260ab7e80717
Dongning Song <mathdsong@gmail.com>
2023-10-19 14:14:11 -0600

    Time: 147 ms (40.05%), Space: 49.2 MB (99.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,16 +3,20 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) {
+    if (x < 0 || x % 10 === 0 && x !== 0) {
         return false;
     } else {
-        let str = String(x);
-        let arr = str.split('');
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] !== arr[arr.length - 1 - i]) {
-                return false;
-            }
+    // reverse the last half of x, and then check if it is the same as the first half
+        let reverseOfLastHalf = 0;
+        while (x > reverseOfLastHalf) {
+           reverseOfLastHalf = reverseOfLastHalf * 10 + x % 10;
+           x = Math.floor(x / 10);
         }
-        return true;
+
+        if (x === reverseOfLastHalf || x === Math.floor(reverseOfLastHalf / 10)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
