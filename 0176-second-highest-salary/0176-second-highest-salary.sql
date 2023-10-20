fb9acc37fccfe7056d58cd0b0e5c073747d64683
Dongning Song <mathdsong@gmail.com>
2023-10-20 13:23:58 -0600

    Time: 402 ms (94.05%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,3 +2,13 @@
 
 SELECT IFNULL((SELECT DISTINCT salary FROM Employee ORDER BY salary DESC LIMIT 1,1), null) AS SecondHighestSalary;
 
+# SELECT salary AS SecondHighestSalary FROM Employee ORDER BY salary DESC LIMIT 1,1;  
+# this was my original solution but can't handle 0 results situation. 
+
+# Key points:
+# IFNULL
+# DISTINCT
+
+# Reference Links:
+# https://www.mysqltutorial.org/mysql-limit.aspx
+# https://www.w3schools.com/mysql/mysql_ifnull.asp
